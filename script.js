1945a924026437730f385eb4855776e1aef1a2d7
leticea <nascimento.leticeah@gmail.com>
2023-01-10 10:08:41 -0300

    codando js

---
+++ script.js
@@ -0,0 +1,14 @@
+const lengthSlider = document.querySelector(".pass-length input");
+const options = document.querySelector(".option input");
+const copyIcon = document.querySelector(".input-box span");
+const passwordInput = document.querySelector(".input-box input");
+const passIndicator = document.querySelector(".pass-indicator");
+const generateBtn = document.querySelector(".generate-btn");
+
+const characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  symbols: "!$%&|[](){}:;.,*+-#@<>~"
+};
+
